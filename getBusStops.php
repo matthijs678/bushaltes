70227a7e5bf284076addacd011f663396887a353
matthijs678 <40187960@roctilburg.nl>
2022-09-28 11:00:24 +0200

    Fetch requests instead of php

--- getBusStops.php
+++ getBusStops.php
@@ -2,18 +2,18 @@
 // sql: init
 $sqli = new mysqli("localhost", "root", "root", "bushaltes", 3306);
 if ($sqli->connect_error) {
-    echo $sqli->connect_error; exit;
+    exit;
 }
 
 // sql: bus
 $stmt = $sqli->prepare("SELECT `cycleSeconds`, `cycleOffsetSeconds` FROM `bus`;");
 if (!$stmt) {
-    echo $sqli->error; exit;
+    exit;
 }
 $stmt->execute();
 $stmt->bind_result($cycleSeconds, $cycleOffsetSeconds);
 if (!$stmt->fetch()) {
-    echo "there is no bus"; exit;
+    exit;
 }
 $stmt->close();
 
@@ -29,7 +29,7 @@ $departure->setTime(0, 0, $secondsNow - $secondsNow % $cycleSeconds, 0);
 // sql: haltes
 $stmt = $sqli->prepare("SELECT `address`, `timeRelativeSeconds` FROM `halte`;");
 if (!$stmt) {
-    echo $sqli->error; exit;
+    exit;
 }
 $stmt->execute();
 $stmt->bind_result($address, $timeRelativeSeconds);
@@ -59,33 +59,5 @@ while ($stmt->fetch()) {
 $stmt->close();
 $sqli->close();
 
-header("Refresh:20");
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div style="display: flex; flex-direction: column; width: 40ch;">
-        <?php
-        for ($i = 0; $i < count($times) - 1; $i++) {
-            $v = $times[$i];
-            echo "<div>" . $v["address"] . "; " . $v["clockTime"] . "</div>";
-        }
-        echo "<div style='margin-top: 1em;'>Aankomt eindbestemming:</div>";
-        $last = $times[count($times) - 1];
-        if ($last) {
-            echo "<div>" . $last["address"] . "; " . $last["clockTime"] ."</div>";
-        }
-        else {
-            echo "<div>Einde bereikt</div>";
-        }
-        ?>
-    </div>
-</body>
-</html>
+// return
+echo json_encode($times);
